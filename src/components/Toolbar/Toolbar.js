6190ba6519b7347eeb90c73ff941d4cdb4bb4ac9
Sebastian Sitja <sebasitjamed@gmail.com>
2020-01-25 23:03:58 -0300

    first commit

---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Toolbar = (props) => (
+    <header>
+        <nav>
+            <div></div>
+            <div></div>
+            <div>
+                <ul>
+                    <li><a>Tareas</a></li>
+                    <li><a>Pr</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+);
